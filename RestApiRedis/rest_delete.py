35976845798ecea621e185929bf5dcdf3e95994c
unknown <satish70210@gmail.com>
2022-02-05 11:33:09 +0530

    delete

--- RestApiRedis/rest_delete.py
+++ RestApiRedis/rest_delete.py
@@ -15,9 +15,15 @@ def read():
     all_data = cur.zrange("data",0,-1,withscores=True)
     return jsonify(all_data)
 
+@app.route('/delete/<int:score>',methods=['DELETE'])
+
+def delete(score):
+    if cur.zremrangebyscore("data",score,score) == 0:
+        return jsonify({"error":"data not found in the key"})
+    else:
+        cur.zremrangebyscore("data",score,score)
+        return read()
+
 if __name__ == '__main__':
-
-
-
     app.run(debug=True,port=5000)
 
